1a2ff65f77119661d817bdc7699f3982da36a473
unknown <justintimber949@gmail.com>
2025-05-29 11:46:19 +0700

    Quartz sync: May 29, 2025, 11:46 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: 'G-VWS875YZFT',
     },
     locale: "en-US",
     baseUrl: "notepreclinic.git",
